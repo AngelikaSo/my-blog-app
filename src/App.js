7c8b58ac62433b4d3971bd721ab6b9b1bbc11fbf
Angelika Sowka <sowkaangelika@gmail.com>
2024-10-15 23:10:40 -0400

    updates-after-feedback

--- src/App.js
+++ src/App.js
@@ -5,17 +5,21 @@ import Posts from "./pages/Posts";
 import ErrorPage from "./pages/ErrorPage";
 import SinglePost from "./pages/SinglePost";
 import postsData from "./data-2.json";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       <BrowserRouter>
+        <Header />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/posts" element={<Posts />} />
           <Route path="*" element={<ErrorPage />} />
           <Route path="/post/:id" element={<SinglePost posts={postsData} />} />
         </Routes>
+        <Footer />
       </BrowserRouter>
     </>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,8 +3,6 @@ import BlogPostList from "../components/BlogPostList";
 import Hero from "../components/Hero";
 import blogPostsData from "../data-2.json";
 import Contact from "../components/Contact";
-import Footer from "../components/Footer";
-import Header from "../components/Header";
 
 const Home = () => {
   const [blogPosts, setBlogPosts] = useState([]);
@@ -15,11 +13,9 @@ const Home = () => {
 
   return (
     <>
-      <Header />
       <Hero />
       {blogPosts.length > 0 && <BlogPostList posts={blogPosts.slice(0, 2)} />}
       <Contact />
-      <Footer />
     </>
   );
 };
